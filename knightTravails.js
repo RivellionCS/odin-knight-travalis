997bf2ebe34a98012b077057b76dc70e730b3689
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-08-17 02:37:38 -0300

    create pseudocode for the knightMoves function

--- knightTravails.js
+++ knightTravails.js
@@ -1,1 +1,9 @@
-function knightMoves(startPosition, endPosition) {}
+function knightMoves(startPosition, endPosition) {
+  // create graph of chessboard squares as [x, y] implicitly
+  // function starts at the startPosition square
+  // check every square connected to the startPosition square as knight movement L-shape
+  // for every connected square we check if it's the endPostion if not we continue until the end position is reached
+  // store the path being taken in an array
+  // return the shortest array which is the one with the least amount of steps
+  // incase of 2 or more equal arrays return the first one
+}
