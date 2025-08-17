3fe91c94c6eba601e4b9a76c8c1172bba9abfcee
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-08-17 03:49:55 -0300

    create a visited array, refactor code into loop

--- knightTravails.js
+++ knightTravails.js
@@ -1,23 +1,39 @@
 function knightMoves(startPosition, endPosition) {
-  // create graph of chessboard squares as [x, y] implicitly fomr start postition
-  let i = startPosition[0];
-  let j = startPosition[1];
-  const neighbours = [
-    [i + 1, j + 2],
-    [i + 1, j - 2],
-    [i + 2, j + 1],
-    [i + 2, j - 1],
-    [i - 1, j + 2],
-    [i - 1, j - 2],
-    [i - 2, j + 1],
-    [i - 2, j - 1],
+  // a queue to store the positions of the knight
+  let queue = [];
+  // an array to check if a position has been visited or not
+  let visited = [
+    [false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false],
   ];
-  // remove any position where i or j is either greater than 7 or less than 0
-  neighbours.forEach((item, index) => {
-    if (item[0] < 0 || item[0] > 7 || item[1] < 0 || item[1] > 7) {
-      neighbours.splice(index, 1);
-    }
-  });
+  while (queue.length > 0) {
+    // create graph of chessboard squares as [x, y] implicitly from start postition
+    let i = startPosition[0];
+    let j = startPosition[1];
+    const neighbours = [
+      [i + 1, j + 2],
+      [i + 1, j - 2],
+      [i + 2, j + 1],
+      [i + 2, j - 1],
+      [i - 1, j + 2],
+      [i - 1, j - 2],
+      [i - 2, j + 1],
+      [i - 2, j - 1],
+    ];
+
+    // remove any position where i or j is either greater than 7 or less than 0
+    neighbours.forEach((item, index) => {
+      if (item[0] < 0 || item[0] > 7 || item[1] < 0 || item[1] > 7) {
+        neighbours.splice(index, 1);
+      }
+    });
+  }
 
   // function starts at the startPosition square
   // check every square connected to the startPosition square as knight movement L-shape
